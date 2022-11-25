1f2cfc3862d0e1f2adb6d819bb5b218f2b2d0fcc
taemt94 <kts8559@kookmin.ac.kr>
2022-11-25 10:31:48 +0900

    Ouster SDK code backup

---
+++ backup_code/ouster_sdk/pcap_to_pcd_ex.py
@@ -0,0 +1,62 @@
+import os
+from contextlib import closing
+from ouster import client, pcap
+
+
+def pcap_to_pcd(source: client.PacketSource,
+                metadata: client.SensorInfo,
+                num: int = 0,
+                pcd_dir: str = ".",
+                pcd_base: str = "pcd_out",
+                pcd_ext: str = "pcd") -> None:
+    "Write scans from a pcap to pcd files (one per lidar scan)."
+
+    if (metadata.format.udp_profile_lidar ==
+            client.UDPProfileLidar.PROFILE_LIDAR_RNG19_RFL8_SIG16_NIR16_DUAL):
+        print("Note: You've selected to convert a dual returns pcap. Second "
+              "returns are ignored in this conversion by this example "
+              "for clarity reasons.  You can modify the code as needed by "
+              "accessing it through github or the SDK documentation.")
+
+    from itertools import islice
+    try:
+        import open3d as o3d  # type: ignore
+    except ModuleNotFoundError:
+        print(
+            "This example requires open3d, which may not be available on all "
+            "platforms. Try running `pip3 install open3d` first.")
+        exit(1)
+
+    if not os.path.exists(pcd_dir):
+        os.makedirs(pcd_dir)
+
+    # precompute xyzlut to save computation in a loop
+    xyzlut = client.XYZLut(metadata)
+
+    # create an iterator of LidarScans from pcap and bound it if num is specified
+    scans = iter(client.Scans(source))
+    if num:
+        scans = islice(scans, num)
+
+    for idx, scan in enumerate(scans):
+
+        xyz = xyzlut(scan.field(client.ChanField.RANGE))
+
+        pcd = o3d.geometry.PointCloud()  # type: ignore
+
+        pcd.points = o3d.utility.Vector3dVector(xyz.reshape(-1,
+                                                            3))  # type: ignore
+
+        pcd_path = os.path.join(pcd_dir, f'{pcd_base}_{idx:06d}.{pcd_ext}')
+        print(f'write frame #{idx} to file: {pcd_path}')
+
+        o3d.io.write_point_cloud(pcd_path, pcd)  # type: ignore
+        
+
+if __name__ == "__main__":
+    with open("/home/tskim/tskim/lidar_test/2022_11_18_16_25_11_os-122211003068.json", "r") as f:
+        metadata = client.SensorInfo(f.read())
+    source = pcap.Pcap("/home/tskim/tskim/lidar_test/2022_11_18_16_25_11_os-122211003068.local.pcap", metadata)
+    
+    with closing(source):
+        pcap_to_pcd(source, metadata, )
